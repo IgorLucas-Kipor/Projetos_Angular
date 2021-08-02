68732bff3c53dff89bdc15748e49e9346c91d914
Igor Lucas <igorescritor@hotmail.com>
2021-08-02 16:01:44 -0300

    Usando String Interpolation e Property Binding

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ServerComponent } from './server/server.component'
 import { AppComponent } from './app.component';
 import { ServersComponent } from './servers/servers.component';
+import { WarningAlertComponent } from './warningAlert/warningAlert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule
